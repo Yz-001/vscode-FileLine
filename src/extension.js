d6b3aadaccb20f37ea7e1da39149892e25f6efcc
yexiaoli <2060804881@qq.com>
2022-09-08 23:10:46 +0800

    feat：把文字改为按钮 增加上界限图标

--- src/extension.js
+++ src/extension.js
@@ -1,11 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+// import { window, StatusBarItem } from 'vscode';
 // import { getConfiguration } from "./configuration.js";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
-const NEED_COMPUTE_TAG=['script','style']
+const NEED_COMPUTE_TAG = ['script', 'style']
+let computeMap = new Map()
+let matchMap = new Map()
+let statusBarObj = {}
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -15,12 +19,35 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 
+	let statusBarEvent=vscode.commands.registerCommand('fileline.range', function (tag='style') {
+		// vscode.window.showInformationMessage('click!!!');
+		gotoDocumentRange(vscode.window.activeTextEditor.document.fileName,tag)
+
+		// 跳转到文件的指定范围
+		function gotoDocumentRange(path,tag){
+			// const path = '/Users/somefile.txt';
+			// const {start,end}=matchMap.get(tag)
+			// console.log('matchMap',matchMap);
+			// vscode.window.activeTextEditor.document.offsetAt(new vscode.Position(100,0))
+			// const options = {
+			// 	// 选中第3行第9列到第3行第17列
+			// 	selection: new vscode.Range(new vscode.Position(...start), new vscode.Position(...end)),
+			// 	// 是否预览，默认true，预览的意思是下次再打开文件是否会替换当前文件
+			// 	preview: false,
+			// 	// 显示在第二个编辑器
+			// 	viewColumn: vscode.ViewColumn.Two
+			// };
+			// vscode.window.showTextDocument(vscode.Uri.file(path), options);
+		}
+	})
+
 	let disposable = vscode.commands.registerCommand('fileline.examine', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 
-		let computeMap=new Map()
+		//alimit a为开头 因为配置是按字母排序的 
+		let islimit = getConfiguration('alimit')
 
 		vscode.window.showInformationMessage('welcome use fileLine plugin!');
 
@@ -30,19 +57,25 @@ function activate(context) {
 		 * @param {*} tag 标签名
 		 * @returns 统计的标签数
 		 */
-		function getTextRegexTagCount(document,tag){
-			let content = document.replace(/\"/g,'\'')
-			let contentList=content.split('\r')
-			let joinStr=``
+		function getTextRegexTagCount(document, tag) {
+			let content = document.replace(/\"/g, '\'')
+			let contentList = content.split('\r')
+			let joinStr = ``
 			for (const index in contentList) {
-				joinStr+=`"${index}${contentList[index]}"+`
+				joinStr += `"${index}${contentList[index]}"+`
 			}
-			const textMatch=joinStr.match(new RegExp(`\<${tag}((.|\n)*?)\<\/${tag}\>`,'ig'))
+			const textMatch = joinStr.match(new RegExp(`\<${tag}((.|\n)*?)\<\/${tag}\>`, 'ig'))
+			const textsearch = joinStr.match(new RegExp(`\<${tag}((.|\n)*?)\<\/${tag}\>`, 'i'))
+
 			// 存在多个相同标签的情况 总数需要叠加
-			if(textMatch){
-				let sum=0
-				for (const index in textMatch) {
-					sum+=textMatch[index].split('\n').length
+			if (textMatch) {
+				let sum = 0
+				for (const matchIndex in textMatch) {
+					const currentSum=textMatch[matchIndex].split('\n').length
+					console.log('textMatch: ', textMatch);
+					sum += currentSum
+					//set
+					matchMap.set(tag,{start:[textsearch.index,0],end:[textsearch.index+currentSum-1,contentList[matchIndex].length]})
 				}
 				return sum
 			}
@@ -54,23 +87,24 @@ function activate(context) {
 		 * @param {*} lineCount 当前文档内容总行数
 		 * @returns 最后bar展示的文字
 		 */
-		async function getBarMessage(document,lineCount){
+		async function getBarMessage(document, lineCount) {
 			// const fileName = document.fileName;
-			//alimit a为开头 因为配置是按字母排序的 
-			let islimit = getConfiguration('alimit')
 
 			// 总数直接取返回文档参数 额外写 
-			let countLimit = getConfiguration('count') && islimit ? `/ ${getConfiguration('count')}`: '';
+			let countLimit = getConfiguration('count') && islimit ? `/ ${getConfiguration('count')}` : '';
 			const lineTotal = lineCount ? `Count: ${lineCount} ${countLimit}` : '';
-			computeMap.set('Count',lineTotal)
-			
-			for (const tag of NEED_COMPUTE_TAG) {
-				// 启用界限配置才会展示指定界限数
-				const tagLimit = getConfiguration(tag) && islimit ? `/ ${getConfiguration(tag)}` : '';
-				const tagTotal = await getTextRegexTagCount(document,tag)
-				const message = tagTotal ? ` ${tag.replace(tag[0],tag[0].toUpperCase())}:${getTextRegexTagCount(document,tag)} ${tagLimit}` :'';
-				computeMap.set(tag,message)
-			}
+			const iconLimit = countLimit && lineTotal>getConfiguration('count')  ? `$(arrow-circle-up)`: '';
+			computeMap.set(`${iconLimit} Count`, lineTotal)
+
+			// for (const tag of NEED_COMPUTE_TAG) {
+			// 	// 启用界限配置才会展示指定界限数
+			// 	const tagLimit = getConfiguration(tag) && islimit ? `/ ${getConfiguration(tag)}` : '';
+			// 	const tagTotal = getTextRegexTagCount(document, tag);
+			// 	//超过界限值 出现icon 
+			// 	const iconLimit = tagLimit && tagTotal>getConfiguration(tag)  ? `$(arrow-circle-up)`: '';
+			// 	const message = tagTotal ? ` ${iconLimit} ${tag.replace(tag[0], tag[0].toUpperCase())}:${tagTotal} ${tagLimit}` : '';
+			// 	computeMap.set(tag, message)
+			// }
 			return [...computeMap.values()].join(" ")
 		}
 
@@ -85,8 +119,9 @@ function activate(context) {
 			}
 
 			let message = ''
-			if(document){
-				message = await getBarMessage(document.getText(),document.lineCount)
+			if (document) {
+				message = await getBarMessage(document.getText(), document.lineCount)
+				setStatusBarBtn(document.getText())
 			}
 
 			// set statusbar
@@ -100,22 +135,48 @@ function activate(context) {
 		}
 
 		// 修改文档内容触发
-		vscode.workspace.onDidChangeTextDocument((activeTextEditor)=>{
+		vscode.workspace.onDidChangeTextDocument((activeTextEditor) => {
 			updateStatusBar(activeTextEditor.document);
 		})
-		
+
 		// 打开文档内容触发
 		vscode.workspace.onDidOpenTextDocument((document) => {
 			updateStatusBar(document);
 		});
 
 		// 获取用户在设置配置数 如果没有设置，返回undefined
-		function getConfiguration(configName){
+		function getConfiguration(configName) {
 			return vscode.workspace.getConfiguration().get(`vscodePluginFileLine.${configName}`);
+		}
+		
+		// 设置底部状态栏按钮
+		function setStatusBarBtn(document) {
+			for (const tag of NEED_COMPUTE_TAG) {
+				// 启用界限配置才会展示指定界限数
+				const tagLimit = getConfiguration(tag) && islimit ? `/ ${getConfiguration(tag)}` : '';
+				const tagTotal = getTextRegexTagCount(document, tag);
+				//超过界限值 出现icon 
+				const iconLimit = tagLimit && tagTotal>getConfiguration(tag)  ? `$(arrow-circle-up)`: '';
+				const message = tagTotal ? `${iconLimit} ${tag.replace(tag[0], tag[0].toUpperCase())}:${tagTotal} ${tagLimit}` : '';
+				
+				// 有对应的值 更新 没有注销  
+				// 没有更新文字和隐藏item api 所以采用注销
+				if(message){
+					if(tag in statusBarObj){
+						statusBarObj[tag].dispose();
+					}
+					statusBarObj[tag] = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
+					statusBarObj[tag].text = message
+					statusBarObj[tag].command = 'fileline.range'
+					statusBarObj[tag].show()
+				}else{
+					statusBarObj[tag].dispose();
+				}
+			}
 		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable,statusBarEvent);
 }
 
 // this method is called when your extension is deactivated
