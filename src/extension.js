6fca99a278e63b7c71102af1c78aa0be8d83eb20
yexla <2060804881@qq.com>
2025-01-25 23:37:40 +0800

    refactor: 1. 修改统计方法; 2. 超限红色颜色展示; 3. 跳转方式优化； 4. 新增template统计 5. 修改 configuration title

--- src/extension.js
+++ src/extension.js
@@ -1,188 +1,317 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-// import { window, StatusBarItem } from 'vscode';
-// import { getConfiguration } from "./configuration.js";
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
-const NEED_COMPUTE_TAG = ['script', 'style']
-let computeMap = new Map()
-let matchMap = new Map()
-let statusBarObj = {}
+// Define the tags that need to be computed, grouped by file type
+const NEED_COMPUTE_TAG = [
+  { lintFileType: "vue", tags: ["template", "script", "style"] },
+];
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+let statusBarObj = {};
+let islimit = false; // Default value, will be set from configuration
 
-	let statusBarEvent=vscode.commands.registerCommand('fileline.range', function (tag='style') {
-		// vscode.window.showInformationMessage('click!!!');
-		gotoDocumentRange(vscode.window.activeTextEditor.document.fileName,tag)
-
-		// 跳转到文件的指定范围
-		function gotoDocumentRange(path,tag){
-			// const path = '/Users/somefile.txt';
-			// const {start,end}=matchMap.get(tag)
-			// console.log('matchMap',matchMap);
-			// vscode.window.activeTextEditor.document.offsetAt(new vscode.Position(100,0))
-			// const options = {
-			// 	// 选中第3行第9列到第3行第17列
-			// 	selection: new vscode.Range(new vscode.Position(...start), new vscode.Position(...end)),
-			// 	// 是否预览，默认true，预览的意思是下次再打开文件是否会替换当前文件
-			// 	preview: false,
-			// 	// 显示在第二个编辑器
-			// 	viewColumn: vscode.ViewColumn.Two
-			// };
-			// vscode.window.showTextDocument(vscode.Uri.file(path), options);
-		}
-	})
-
-	let disposable = vscode.commands.registerCommand('fileline.examine', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-
-		//alimit a为开头 因为配置是按字母排序的 
-		let islimit = getConfiguration('alimit')
-
-		vscode.window.showInformationMessage('welcome use fileLine plugin!');
-
-		/**
-		 * 根据文档内容，获取标签总数
-		 * @param {*} document 当前文档内容
-		 * @param {*} tag 标签名
-		 * @returns 统计的标签数
-		 */
-		function getTextRegexTagCount(document, tag) {
-			let content = document.replace(/\"/g, '\'')
-			let contentList = content.split('\r')
-			let joinStr = ``
-			for (const index in contentList) {
-				joinStr += `"${index}${contentList[index]}"+`
-			}
-			const textMatch = joinStr.match(new RegExp(`\<${tag}((.|\n)*?)\<\/${tag}\>`, 'ig'))
-			const textsearch = joinStr.match(new RegExp(`\<${tag}((.|\n)*?)\<\/${tag}\>`, 'i'))
-
-			// 存在多个相同标签的情况 总数需要叠加
-			if (textMatch) {
-				let sum = 0
-				for (const matchIndex in textMatch) {
-					const currentSum=textMatch[matchIndex].split('\n').length
-					console.log('textMatch: ', textMatch);
-					sum += currentSum
-					//set
-					matchMap.set(tag,{start:[textsearch.index,0],end:[textsearch.index+currentSum-1,contentList[matchIndex].length]})
-				}
-				return sum
-			}
-		}
-
-		/**
-		 * 统计最新状态栏文字
-		 * @param {*} document 当前文档内容
-		 * @param {*} lineCount 当前文档内容总行数
-		 * @returns 最后bar展示的文字
-		 */
-		async function getBarMessage(document, lineCount) {
-			// const fileName = document.fileName;
-
-			// 总数直接取返回文档参数 额外写 
-			let countLimit = getConfiguration('count') && islimit ? `/ ${getConfiguration('count')}` : '';
-			const lineTotal = lineCount ? `Count: ${lineCount} ${countLimit}` : '';
-			const iconLimit = countLimit && lineTotal>getConfiguration('count')  ? `$(arrow-circle-up)`: '';
-			computeMap.set(`${iconLimit} Count`, lineTotal)
-
-			// for (const tag of NEED_COMPUTE_TAG) {
-			// 	// 启用界限配置才会展示指定界限数
-			// 	const tagLimit = getConfiguration(tag) && islimit ? `/ ${getConfiguration(tag)}` : '';
-			// 	const tagTotal = getTextRegexTagCount(document, tag);
-			// 	//超过界限值 出现icon 
-			// 	const iconLimit = tagLimit && tagTotal>getConfiguration(tag)  ? `$(arrow-circle-up)`: '';
-			// 	const message = tagTotal ? ` ${iconLimit} ${tag.replace(tag[0], tag[0].toUpperCase())}:${tagTotal} ${tagLimit}` : '';
-			// 	computeMap.set(tag, message)
-			// }
-			return [...computeMap.values()].join(" ")
-		}
-
-		/**
-		 * 更新状态栏文字
-		 * @param {*} document 当前文档内容
-		 */
-		async function updateStatusBar(document) {
-			// no file return 
-			if (!vscode.window.activeTextEditor) {
-				return;
-			}
-
-			let message = ''
-			if (document) {
-				message = await getBarMessage(document.getText(), document.lineCount)
-				setStatusBarBtn(document.getText())
-			}
-
-			// set statusbar
-			vscode.window.setStatusBarMessage(message);
-		}
-
-
-		//初始化状态栏 没有文件情况下不展示
-		if (vscode.window.activeTextEditor) {
-			updateStatusBar(vscode.window.activeTextEditor.document)
-		}
-
-		// 修改文档内容触发
-		vscode.workspace.onDidChangeTextDocument((activeTextEditor) => {
-			updateStatusBar(activeTextEditor.document);
-		})
-
-		// 打开文档内容触发
-		vscode.workspace.onDidOpenTextDocument((document) => {
-			updateStatusBar(document);
-		});
-
-		// 获取用户在设置配置数 如果没有设置，返回undefined
-		function getConfiguration(configName) {
-			return vscode.workspace.getConfiguration().get(`vscodePluginFileLine.${configName}`);
-		}
-		
-		// 设置底部状态栏按钮
-		function setStatusBarBtn(document) {
-			for (const tag of NEED_COMPUTE_TAG) {
-				// 启用界限配置才会展示指定界限数
-				const tagLimit = getConfiguration(tag) && islimit ? `/ ${getConfiguration(tag)}` : '';
-				const tagTotal = getTextRegexTagCount(document, tag);
-				//超过界限值 出现icon 
-				const iconLimit = tagLimit && tagTotal>getConfiguration(tag)  ? `$(arrow-circle-up)`: '';
-				const message = tagTotal ? `${iconLimit} ${tag.replace(tag[0], tag[0].toUpperCase())}:${tagTotal} ${tagLimit}` : '';
-				
-				// 有对应的值 更新 没有注销  
-				// 没有更新文字和隐藏item api 所以采用注销
-				if(message){
-					if(tag in statusBarObj){
-						statusBarObj[tag].dispose();
-					}
-					statusBarObj[tag] = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
-					statusBarObj[tag].text = message
-					statusBarObj[tag].command = 'fileline.range'
-					statusBarObj[tag].show()
-				}else{
-					statusBarObj[tag].dispose();
-				}
-			}
-		}
-	});
-
-	context.subscriptions.push(disposable,statusBarEvent);
+function getConfiguration(configName) {
+  return vscode.workspace
+    .getConfiguration()
+    .get(`vscodePluginFileLine.${configName}`);
 }
 
-// this method is called when your extension is deactivated
-function deactivate() { }
+// Function to clear all status bars
+function clearAllStatusBars() {
+  Object.keys(statusBarObj).forEach((key) => {
+    disposeStatusBarItem(key);
+  });
+}
+
+// Function to dispose a single status bar item
+function disposeStatusBarItem(key) {
+  statusBarObj[key]?.dispose();
+  delete statusBarObj[key];
+}
+
+function activate(context) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("fileline.examine", () => {
+      islimit = getConfiguration("alimit");
+      vscode.window.showInformationMessage("Welcome to use fileLine plugin!");
+      updateStatusBar(vscode.window.activeTextEditor?.document);
+    })
+  );
+
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (
+      event.affectsConfiguration("vscodePluginFileLine.alimit") ||
+      event.affectsConfiguration("vscodePluginFileLine.count") ||
+      event.affectsConfiguration("vscodePluginFileLine.template") ||
+      event.affectsConfiguration("vscodePluginFileLine.script") ||
+      event.affectsConfiguration("vscodePluginFileLine.style")
+    ) {
+      islimit = getConfiguration("alimit");
+      updateStatusBar(vscode.window.activeTextEditor?.document);
+    }
+  });
+
+  function updateStatusBar(document) {
+    if (!document) {
+      clearAllStatusBars(); // Clear all status bars when no document is active
+      return;
+    }
+
+    if (document.fileName.endsWith(".vue")) {
+      setStatusBarBtn(document, context);
+    } else {
+      handleNonVueDocument(document);
+    }
+  }
+
+  function handleNonVueDocument(document) {
+    const countLimit = getNumericConfiguration("count");
+    const lineTotal = `Count: ${document.lineCount}`;
+    if (islimit && countLimit !== null) {
+      const messageWithLimit = `${lineTotal} / ${countLimit}`;
+      const icon = "$(arrow-circle-up)";
+      const fullMessage = `${icon} ${messageWithLimit}`;
+      updateOrCreateStatusBarItem(
+        "count",
+        fullMessage,
+        document.lineCount > countLimit ? "red" : undefined
+      );
+      clearNonCountStatusBars(); // Clear non-count status bars
+    } else {
+      updateOrCreateStatusBarItem("count", lineTotal);
+      clearNonCountStatusBars();
+    }
+  }
+
+  function setStatusBarBtn(document, context) {
+    const applicableTags =
+      NEED_COMPUTE_TAG.find((item) => item.lintFileType === "vue")?.tags || [];
+
+    // First handle Count status bar item
+    const countLimit = getNumericConfiguration("count");
+    const lineTotal = `Count: ${document.lineCount}`;
+    if (islimit && countLimit !== null) {
+      const messageWithLimit = `${lineTotal} / ${countLimit}`;
+      const icon = "$(arrow-circle-up)";
+      const fullMessage = `${icon} ${messageWithLimit}`;
+      updateOrCreateStatusBarItem(
+        "count",
+        fullMessage,
+        document.lineCount > countLimit ? "red" : undefined
+      );
+    } else {
+      updateOrCreateStatusBarItem("count", lineTotal);
+    }
+
+    for (const tag of applicableTags) {
+      const tagTotal = getTextRegexTagCount(document, tag);
+      if (tagTotal > 0) {
+        if (islimit) {
+          const tagLimit = getNumericConfiguration(tag);
+          if (tagLimit !== null) {
+            const message = `${tag.replace(
+              tag[0],
+              tag[0].toUpperCase()
+            )}:${tagTotal}/${tagLimit}`;
+            const isOverLimit = tagTotal > tagLimit;
+            const iconForTag = isOverLimit ? "$(arrow-circle-up)" : "";
+            const fullMessageForTag = iconForTag
+              ? `${iconForTag} ${message}`
+              : message;
+            createOrUpdateSBItemWithCmd(
+              tag,
+              fullMessageForTag,
+              document,
+              tag,
+              isOverLimit ? "red" : undefined,
+              context
+            );
+          } else {
+            const message = `${tag.replace(
+              tag[0],
+              tag[0].toUpperCase()
+            )}:${tagTotal}`;
+            createOrUpdateSBItemWithCmd(
+              tag,
+              message,
+              document,
+              tag,
+              undefined,
+              context
+            );
+          }
+        } else {
+          const message = `${tag.replace(
+            tag[0],
+            tag[0].toUpperCase()
+          )}:${tagTotal}`;
+          createOrUpdateSBItemWithCmd(
+            tag,
+            message,
+            document,
+            tag,
+            undefined,
+            context
+          );
+        }
+      }
+    }
+  }
+
+  function createOrUpdateSBItemWithCmd(
+    key,
+    message,
+    document,
+    tag,
+    color,
+    context
+  ) {
+    if (!statusBarObj[key]) {
+      statusBarObj[key] = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left
+      );
+      statusBarObj[key].show();
+    }
+    statusBarObj[key].text = message;
+    statusBarObj[key].color = color;
+
+    const commandName = `fileline.jumpTo${tag
+      .charAt(0)
+      .toUpperCase()}${tag.slice(1)}`;
+
+    // Check if the command already exists before registering it
+    vscode.commands.getCommands(true).then((commands) => {
+      if (!commands.includes(commandName)) {
+        context.subscriptions.push(
+          vscode.commands.registerCommand(commandName, () => {
+            jumpToTagBlock(document, tag);
+          })
+        );
+      }
+      statusBarObj[key].command = commandName;
+    });
+  }
+
+  function jumpToTagBlock(document, tag) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    const text = document.getText();
+    const startPattern = new RegExp(`<${tag}[^>]*>`, "gi");
+    let match;
+    let targetStartIndex = -1;
+    while ((match = startPattern.exec(text)) !== null) {
+      if (targetStartIndex === -1) {
+        targetStartIndex = match.index;
+      }
+    }
+
+    if (targetStartIndex === -1) return;
+
+    const startPosition = document.positionAt(targetStartIndex);
+
+    const endPattern = new RegExp(`</${tag}>`, "i");
+    const remainingText = text.substring(targetStartIndex);
+    const endIndexRelativeToStart = remainingText.search(endPattern);
+    let endPosition;
+    if (endIndexRelativeToStart !== -1) {
+      const endIndex = targetStartIndex + endIndexRelativeToStart;
+      endPosition = document.positionAt(endIndex);
+    } else {
+      endPosition = startPosition;
+    }
+
+    const selection = new vscode.Selection(startPosition, startPosition);
+    editor.selection = selection;
+    editor.revealRange(selection);
+  }
+
+  function clearNonCountStatusBars() {
+    Object.keys(statusBarObj).forEach((key) => {
+      if (key !== "count") {
+        disposeStatusBarItem(key);
+      }
+    });
+  }
+
+  function updateOrCreateStatusBarItem(key, message, color) {
+    if (!statusBarObj[key]) {
+      statusBarObj[key] = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left
+      );
+      statusBarObj[key].show();
+    }
+    statusBarObj[key].text = message;
+    statusBarObj[key].color = color;
+  }
+
+  function getTextRegexTagCount(document, tag) {
+    let content = document.getText();
+    if (!content) {
+      console.error("Document content is empty.");
+      return 0;
+    }
+    if (document.fileName.endsWith(".vue")) {
+      const vueBlocks = {
+        template: /<template[^>]*>([\s\S]*?)<\/template>/gi,
+        script: /<script[^>]*>([\s\S]*?)<\/script>/gi,
+        style: /<style[^>]*>([\s\S]*?)<\/style>/gi,
+      };
+      const blockContentMatch = content.match(vueBlocks[tag]);
+      if (blockContentMatch && Array.isArray(blockContentMatch)) {
+        let sum = 0;
+        for (const block of blockContentMatch) {
+          sum += block.split("\n").length;
+        }
+        return sum;
+      }
+    }
+    return 0;
+  }
+
+  function getNumericConfiguration(configName) {
+    if (!islimit) return null;
+    const configValue = getConfiguration(configName);
+    const numericValue = Number(configValue);
+    return isNaN(numericValue) || configValue === "" ? null : numericValue;
+  }
+
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    if (
+      vscode.window.activeTextEditor &&
+      event.document === vscode.window.activeTextEditor.document
+    ) {
+      updateStatusBar(event.document);
+    }
+  });
+
+  vscode.workspace.onDidOpenTextDocument((document) => {
+    if (
+      vscode.window.activeTextEditor &&
+      document === vscode.window.activeTextEditor.document
+    ) {
+      updateStatusBar(document);
+    }
+  });
+
+  vscode.window.onDidChangeActiveTextEditor((editor) => {
+    if (editor) {
+      updateStatusBar(editor.document);
+    } else {
+      clearAllStatusBars(); // Clear all status bars when no editor is active
+    }
+  });
+
+  if (!vscode.window.activeTextEditor) {
+    clearAllStatusBars(); // Ensure all status bars are cleared on activation if no editor is active
+  } else {
+    updateStatusBar(vscode.window.activeTextEditor.document);
+  }
+}
+
+function deactivate() {
+  clearAllStatusBars(); // Clean up all status bars on deactivation
+}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
